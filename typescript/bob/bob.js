e72606203a25840b9102179f5edebade3613c484
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2023-02-03 14:49:13 +0100

    Bob

---
+++ typescript/bob/bob.js
@@ -0,0 +1,14 @@
+export function hey(message) {
+    let answer = "";
+    let onlyLetters = message.trim();
+    if (onlyLetters.length === 0)
+        return "Fine. Be that way!";
+    else if (/[A-Z]/.test(onlyLetters) && onlyLetters === onlyLetters.toUpperCase())
+        return onlyLetters.endsWith("?")
+            ? "Calm down, I know what I'm doing!"
+            : "Whoa, chill out!";
+    else if (onlyLetters.endsWith("?"))
+        return "Sure.";
+    else
+        return "Whatever.";
+}
---
+++ typescript/dnd-character/dnd-character.ts
@@ -0,0 +1,20 @@
+export class DnDCharacter {
+
+  let strength: number;
+  dexterity: number;
+  constitution: number;
+  intelligence: number;
+  widsom: number;
+  charisma: number;
+
+  let diceResult = Math.floor(Math.random() * 6);
+
+
+  public static generateAbilityScore(): number {
+    
+  }
+
+  public static getModifierFor(abilityValue: number): number {
+    
+  }
+}
