b773e7b2e07356e962648103428184163eac73a0
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2023-02-10 10:41:02 +0100

    resistor-color-duo

---
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -0,0 +1,9 @@
+//
+// This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+const COLORS =["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
+
+export const decodedValue = ([color1, color2]) => {
+  return (COLORS.indexOf(color1)*10 + COLORS.indexOf(color2));
+};
