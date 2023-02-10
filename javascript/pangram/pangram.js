58eac88722592f3a91774ddca5c0f920e24fe8e0
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2023-02-10 13:35:20 +0100

    pangram

---
+++ javascript/pangram/pangram.js
@@ -0,0 +1,11 @@
+//
+// This is only a SKELETON file for the 'Pangram' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+export const isPangram = (input) => {
+  let inputArray = input.toLowerCase().split("");
+
+  return alphabet.every(letter => inputArray.includes(letter));
+};
